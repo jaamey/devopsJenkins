fec5b0c2411bb69d8e931df96041e6e54f6a9ca2
jaamey <jaamey@gmail.com>
2020-05-01 20:06:07 +0000

    dsl docker configuration

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/jaamey/devopsJenkins') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('jaamey')
+            node / gitConfigEmail('jaamey@gmail.com')
         }
     }
     triggers {
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('ameyjocker/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('dockeHubCred')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
