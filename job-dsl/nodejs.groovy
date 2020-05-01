d9f9f423c1404bc11d52ceba6f1b2b3dfbc3d574
jaamey <jaamey@gmail.com>
2020-05-01 18:14:48 +0000

    upd. dsl

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/jaamey/devopsJenkins/tree/master/job-dsl') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('jaamey')
+            node / gitConfigEmail('jaamey@gmail.com')
         }
     }
     triggers {
